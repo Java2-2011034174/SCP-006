62394114ec9eadd0326a5f0036d65a0634d0f7aa
SeongMin Cho <librarian17@naver.com>
2016-11-19 07:06:28 +0000

    first commit

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
     
 	has_many :comments
 	has_many :menus
-	has_many :restaurant_foodtypes
+	has_many :restaurant_foodtypes	
 
     geocoded_by :address
     after_validation :geocode
